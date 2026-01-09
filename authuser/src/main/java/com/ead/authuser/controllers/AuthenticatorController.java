5d6300b52aee81aa7455e47110b4ebd26261b3b7
Rafael <80908824@telefonicati.onmicrosoft.com>
2026-01-09 12:32:35 -0300

    Configurção de logs pelo Spring

--- authuser/src/main/java/com/ead/authuser/controllers/AuthenticatorController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticatorController.java
@@ -3,19 +3,19 @@ package com.ead.authuser.controllers;
 import com.ead.authuser.dtos.UserRecordDTO;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthenticatorController {
 
+    Logger logger = LoggerFactory.getLogger(AuthenticatorController.class);
     final UserService userService;
 
     public AuthenticatorController(UserService userService) {
@@ -34,5 +34,15 @@ public class AuthenticatorController {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: E-mail is Already Taken!");
         }
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.registerUser(userRecordDTO));
+    }
+
+    @GetMapping("/logs")
+    public String index(){
+        logger.trace("TRACE");
+        logger.debug("DEBUG");
+        logger.info("INFO");
+        logger.warn("WARNING");
+        logger.error("ERROR");
+        return "Logging Spring Boot ...";
     }
 }
