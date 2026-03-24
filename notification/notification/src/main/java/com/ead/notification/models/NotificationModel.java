23e2f647554eb4e4b6ea69be82218035c957e246
Rafael <80908824@telefonicati.onmicrosoft.com>
2026-03-23 22:29:21 -0300

    Configuracao inicial Notification

---
+++ notification/notification/src/main/java/com/ead/notification/models/NotificationModel.java
@@ -0,0 +1,40 @@
+package com.ead.notification.models;
+
+import com.ead.notification.enums.NotificationStatus;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "TB_NOTIFICATIONS")
+public class NotificationModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID notificationId;
+    @Column(nullable = false)
+    private UUID userId;
+    @Column(nullable = false, length = 150)
+    private String titlle;
+    @Column(nullable = false)
+    private String message;
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private NotificationStatus notificationStatus;
+}
